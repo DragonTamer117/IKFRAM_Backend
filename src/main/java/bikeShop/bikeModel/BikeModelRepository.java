3d01b3c7f041fbad7f5dd29955f2d88501d4a692
Loek Appel <loek@appel-snijder.nl>
2023-04-28 18:56:14 +0200

    clean up

--- src/main/java/bikeShop/bikeModel/BikeModelRepository.java
+++
@@ -1,9 +0,0 @@
-package bikeShop.bikeModel;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface BikeModelRepository extends JpaRepository<BikeModel, Long> {
-    Optional<BikeModel> findBikeModelByBikeModelId(Long bikeModelId);
-}
--- src/main/java/bikeShop/bikeOrder/BikeOrderRepository.java
+++
@@ -1,9 +0,0 @@
-package bikeShop.bikeOrder;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface BikeOrderRepository extends JpaRepository<BikeOrder, Long> {
-    Optional<BikeOrder> findBikeOrderByOrderId(Long bikeOrderId);
-}
