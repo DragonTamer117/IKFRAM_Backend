65e5f2015fbe011078df98276d28fa0e0884e5ce
Loek Appel <loek@appel-snijder.nl>
2023-04-30 12:23:42 +0200

    Created Services, Controller and Repositories for the models that didn't have them

---
+++ src/main/java/application/models/storage_products/StorageProductController.java
@@ -0,0 +1,13 @@
+package application.models.storage_products;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/storageProducts")
+public class StorageProductController {
+}
---
+++ src/main/java/application/models/storage_products/StorageProductService.java
@@ -0,0 +1,9 @@
+package application.models.storage_products;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StorageProductService {
+}
