343fb2dd05a386c1d65620b7b69daaa949549cf0
Ali Basiri <57348384+DragonTamer117@users.noreply.github.com>
2023-05-15 11:39:12 +0200

    All storage products can be GET, a specific one can be GET and a storage product can be created. StorageProductDTO gives only the data we want to give the frontend.

--- src/main/java/application/dtos/StorageProductDTO.java
+++ src/main/java/application/dtos/StorageProductDTO.java
@@ -1,4 +1,20 @@
 package application.dtos;
 
+import application.models.categories.Category;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import lombok.Data;
+
+import javax.persistence.JoinColumn;
+
+@Data
 public class StorageProductDTO {
+    private String name;
+    @JoinColumn(name = "category_name")
+    @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "name")
+    private Category category;
+    private double price;
+    private boolean inStock;
+    private boolean isSoldOut;
+    private String imageUrl;
 }
--- src/main/java/application/models/storage_products/StorageProductService.java
+++ src/main/java/application/models/storage_products/StorageProductService.java
@@ -1,9 +1,37 @@
 package application.models.storage_products;
 
+import application.dtos.StorageProductDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class StorageProductService {
+    private final StorageProductRepository storageProductRepository;
+
+    public Optional<StorageProduct> findById(UUID id) {
+        return storageProductRepository.findById(id);
+    }
+
+    public List<StorageProduct> findAll() {
+        return storageProductRepository.findAll();
+    }
+
+    public StorageProduct create(StorageProductDTO storageProductDTO) {
+        StorageProduct storageProduct = StorageProduct.builder()
+                .name(storageProductDTO.getName())
+                .price(storageProductDTO.getPrice())
+                .inStock(true)
+                .imageUrl(storageProductDTO.getImageUrl())
+                .category(storageProductDTO.getCategory())
+                .build();
+
+        storageProductRepository.save(storageProduct);
+
+        return storageProduct;
+    }
 }
