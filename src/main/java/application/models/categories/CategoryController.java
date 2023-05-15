eef8d308793c5149cec4c685c73d87b733928b80
Ali Basiri <57348384+DragonTamer117@users.noreply.github.com>
2023-05-15 11:37:50 +0200

    Small changes to CategoryController.java

--- src/main/java/application/models/categories/CategoryController.java
+++ src/main/java/application/models/categories/CategoryController.java
@@ -1,8 +1,11 @@
 package application.models.categories;
 
 import application.dtos.CategoryDTO;
+import application.models.users.UserService;
 import com.zhaofujun.automapper.AutoMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,25 +19,41 @@ import java.util.stream.Collectors;
 public class CategoryController {
     private final AutoMapper mapper = new AutoMapper();
     private final CategoryService categoryService;
+    private final UserService userService;
 
     @CrossOrigin
     @GetMapping("/{id}")
-    public CategoryDTO find(@PathVariable("id") UUID id) {
+    public ResponseEntity<CategoryDTO> find(@RequestHeader("Authorization") String bearerToken, @PathVariable("id") UUID id) {
         Category category = categoryService.findById(id);
-        return mapper.map(category, CategoryDTO.class);
+
+        if (userService.isAllowedRole(bearerToken)) {
+            return ResponseEntity.ok(mapper.map(category, CategoryDTO.class));
+        }
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 
     @CrossOrigin
     @PostMapping
-    public Category create(@RequestHeader("Authorization") String bearerToken, @RequestBody CategoryDTO categoryDTO) {
-        return categoryService.createCategory(categoryDTO.getName());
+    public ResponseEntity<Category> create(@RequestHeader("Authorization") String bearerToken, @RequestBody CategoryDTO categoryDTO) {
+        if (userService.isAllowedRole(bearerToken)) {
+            return ResponseEntity.ok(categoryService.createCategory(categoryDTO.getName()));
+        }
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 
     @CrossOrigin
     @GetMapping("/findAll")
-    public List<CategoryDTO> findAllCategories() {
-        return categoryService.findAllCategories().stream()
-                .map(category -> mapper.map(category, CategoryDTO.class))
-                .collect(Collectors.toList());
+    @CrossOrigin(origins = "http://localhost:7700")
+    public ResponseEntity<List<CategoryDTO>> findAllCategories(@RequestHeader("Authorization") String bearerToken) {
+        System.out.println("CategoryDTO: " + bearerToken);
+        if (userService.isAllowedRole(bearerToken)) {
+            return ResponseEntity.ok(categoryService.findAllCategories().stream()
+                    .map(category -> mapper.map(category, CategoryDTO.class))
+                    .collect(Collectors.toList()));
+        }
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 }
